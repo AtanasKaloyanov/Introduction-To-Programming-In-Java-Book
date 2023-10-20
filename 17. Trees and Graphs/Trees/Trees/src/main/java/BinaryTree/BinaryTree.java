a62c507fe938ccecd7af4b71d649c1502b5b257f
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-10-20 23:14:40 +0300

    Binary Tree example

---
+++ 17. Trees and Graphs/Trees/Trees/src/main/java/BinaryTree/BinaryTree.java
@@ -0,0 +1,93 @@
+package BinaryTree;
+
+public class BinaryTree<T> {
+    public static class BinaryTreeNode<T> {
+        private T value;
+        private boolean hasPerant;
+        private BinaryTreeNode<T> leftChild;
+        private BinaryTreeNode<T> rightChild;
+
+        public BinaryTreeNode(T value, BinaryTreeNode<T> leftChild, BinaryTreeNode<T> rightChild) {
+            if (value == null) {
+                throw new IllegalArgumentException("Cannot insert null value");
+            }
+            this.value = value;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+        }
+
+        public BinaryTreeNode(T value) {
+            this(value, null, null);
+        }
+
+        public T getValue() {
+            return this.value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public BinaryTreeNode<T> getLeftChild() {
+            return this.leftChild;
+        }
+
+        public void setLeftChild(BinaryTreeNode<T> leftChild) {
+            if (leftChild == null || leftChild.hasPerant) {
+                throw new IllegalArgumentException();
+            }
+
+            this.leftChild = leftChild;
+            leftChild.hasPerant = true;
+        }
+
+        public BinaryTreeNode<T> getRightChild() {
+            return this.rightChild;
+        }
+
+        public void setRightChild(BinaryTreeNode<T> rightChild) {
+            if (rightChild == value || rightChild.hasPerant) {
+                throw new IllegalArgumentException();
+            }
+
+            this.rightChild = rightChild;
+            rightChild.hasPerant = true;
+        }
+
+    }
+
+    private BinaryTreeNode<T> root;
+
+    public BinaryTree(T value, BinaryTree<T> leftChild, BinaryTree<T> rightChild) {
+        BinaryTreeNode<T> left = leftChild != null ? leftChild.root : null;
+        BinaryTreeNode<T> right = rightChild != null ? rightChild.root : null;
+
+        this.root = new BinaryTreeNode<T>(value, left, right);
+    }
+
+    public BinaryTree(T value) {
+        this(value, null, null);
+    }
+
+    public BinaryTreeNode<T> getRoot() {
+        return this.root;
+    }
+
+    public BinaryTreeNode<T> getLeftNode() {
+        if (this.root == null) {
+            return null;
+        }
+
+        return this.root.leftChild;
+    }
+
+    public BinaryTreeNode<T> getRightNode() {
+        if (this.root == null) {
+            return null;
+        }
+
+        return this.root.rightChild;
+    }
+
+    
+}
---
+++ 17. Trees and Graphs/Trees/Trees/src/main/java/BinaryTree/Main.java
@@ -0,0 +1,10 @@
+package BinaryTree;
+
+public class Main {
+    public static void main(String[] args) {
+        // pre-order = left, root, right
+        // in-order = root, left, right
+        // post-order = left, right, root
+    }
+
+}
