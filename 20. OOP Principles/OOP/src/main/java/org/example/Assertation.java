02117dfe1a5fb636874e85bb77db1f8391feebeb
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-11-06 23:40:10 +0200

    Java Assertations example

---
+++ 20. OOP Principles/OOP/src/main/java/org/example/Assertation.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Assertation {
+    public static void main(String[] args) {
+        int value = 15;
+        assert value >= 18 : "Not val";
+        System.out.println("value is: " + value);
+    }
+}
--- 20. OOP Principles/OOP/src/main/java/org/example/Main.java
+++ 20. OOP Principles/OOP/src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         AfricanLion africanLion = new AfricanLion(true, 100);
         System.out.println(africanLion);
