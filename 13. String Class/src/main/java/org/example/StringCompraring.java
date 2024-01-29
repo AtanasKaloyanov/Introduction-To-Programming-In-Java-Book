544d2413f43338845fcc112ade1fe008e76802c2
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-01-29 23:50:59 +0200

    String project

---
+++ 13. String Class/src/main/java/org/example/StringCompraring.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class StringCompraring {
+    public static void main(String[] args) {
+        String first = "Ana";
+        String second = "Ana";
+        System.out.println(first == second);
+    }
+}
