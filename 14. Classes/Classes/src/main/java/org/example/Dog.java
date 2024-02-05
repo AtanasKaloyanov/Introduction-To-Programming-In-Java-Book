b3afe577fd08cc16fef236a761f341a6eddb6210
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-02-05 23:35:57 +0200

    Classes theme

---
+++ 14. Classes/Classes/src/main/java/org/example/Dog.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Dog {
+     private String name;
+     private int age;
+     public Dog(String name, int age) {
+          this.name = name;
+          this.age = age;
+     }
+
+     public Dog() {
+
+     }
+
+     public String getName() {
+          return this.name;
+     }
+
+     public int getAge() {
+          return this.age;
+     }
+}
+
+
---
+++ 14. Classes/Classes/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)   {
+        Dog myDog = new Dog("a", 10);
+        System.out.println(myDog.getName());
+        System.out.println(myDog.getAge());
+    }
+}
